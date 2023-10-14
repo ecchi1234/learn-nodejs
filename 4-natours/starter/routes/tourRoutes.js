e3676132aa0322c9ef7ed4ff58065a039bbf3379
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2023-10-14 17:22:18 +0700

    chainning middle

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -5,10 +5,15 @@ const router = express.Router();
 
 router.param('id', tourController.checkInvalidId);
 
+// create a check body middleware
+// check if body contains the name and price property
+// If not send back 400 (bad request)
+// Add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkValidBody, tourController.createTour);
 
 router
   .route('/:id')
