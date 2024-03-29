53b7c5fd5b9d88cdad3c6a09d1b0c812de00a8f1
chinn-nws <ngocchi.fit148.vnu@gmail.com>
2024-03-29 21:19:09 +0700

    do come coding

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connect successfully!');
+  });
 
 const app = require('./app');
 
