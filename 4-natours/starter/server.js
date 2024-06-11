3537d402795864ca637da1a36082e5b8ac2ebd57
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2024-06-11 14:50:41 +0700

    122: handling unhandled rejection

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -25,7 +25,15 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
